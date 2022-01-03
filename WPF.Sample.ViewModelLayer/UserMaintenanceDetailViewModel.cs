a9959eae92dd0e8d83ccb9b302a7f30a67924f2a
Cuong Ta Quang <cuong@zyca.vn>
2022-01-03 09:55:25 +0700

    ok

---
+++ WPF.Sample.ViewModelLayer/UserMaintenanceDetailViewModel.cs
@@ -0,0 +1,30 @@
+﻿using WPF.Sample.DataLayer;
+
+namespace WPF.Sample.ViewModelLayer
+{
+  public class UserMaintenanceDetailViewModel : UserMaintenanceListViewModel
+  {
+    private User _Entity = new User();
+
+    public User Entity
+    {
+      get { return _Entity; }
+      set {
+        _Entity = value;
+        RaisePropertyChanged("Entity");
+      }
+    }
+
+    public override void LoadUsers()
+    {
+      // Load all users
+      base.LoadUsers();
+
+      // Set default user
+      if (Users.Count > 0)
+      {
+        Entity = Users[0];
+      }
+    }
+  }
+}
---
+++ WPF.Sample/UserControls/UserMaintenanceControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using WPF.Sample.ViewModelLayer;
+
+namespace WPF.Sample.UserControls
+{
+  public partial class UserMaintenanceControl : UserControl
+  {
+    public UserMaintenanceControl()
+    {
+      InitializeComponent();
+
+      // Connect to instance of the view model created by the XAML
+      _viewModel = (UserMaintenanceViewModel)this.Resources["viewModel"];
+    }
+
+    private UserMaintenanceViewModel _viewModel = null;
+
+    private void CloseButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+      _viewModel.Close();
+    }
+
+    private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+    {
+      _viewModel.LoadUsers();
+    }
+  }
+}
